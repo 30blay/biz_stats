205859e1d8f5f6ca84e1f64f026e7fb1c7b2dee5
Étienne Tremblay <45673646+30blay@users.noreply.github.com>
2020-04-23 12:04:23 -0400

    add basic auth

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import inspect
 import flask
 from flask import request, jsonify
 import connexion
-from etl.DataWarehouse import DataWarehouse
+from etl.DataWarehouse import DataWarehouse, WarehouseMode
 from etl.Metric import *
 import datetime as dt
 from etl.date_utils import PeriodType
@@ -25,6 +25,14 @@ allowed_metrics = [
 ]
 
 allowed_metrics_dict = dict(zip([str(m) for m in allowed_metrics], allowed_metrics))
+
+
+def basic_auth(username, password, required_scopes=None):
+    if username == 'admin' and password == 'secret':
+        return {'sub': 'admin'}
+
+    # optional: raise exception for custom error response
+    return None
 
 
 def slice_feed(feed_code, metrics, period_type, start, stop):
@@ -56,9 +64,9 @@ def slice_metric(metric, period_type, start, stop):
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     # Create the application instance
-    app = connexion.App(__name__, specification_dir='./')
+    app = connexion.App(__name__, specification_dir='./', host='0.0.0.0', port=5000)
 
     # Read the swagger.yml file to configure the endpoints
     app.add_api('swagger.yml')
-
-    app.run(debug=True)
+    debug = warehouse.mode == WarehouseMode.DEVELOPMENT
+    app.run(debug=debug)
